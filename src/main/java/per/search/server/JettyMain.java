57dcaf742d3ad59619d885d101bec86ba34f6843
johnlreyes <JohnLReyes@yahoo.com>
2011-09-28 01:18:49 +0800

    update jetty codes

--- src/main/java/per/search/server/JettyMain.java
+++ src/main/java/per/search/server/JettyMain.java
@@ -1,17 +1,30 @@
 package per.search.server;
 
-import java.io.File;
-
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.xml.XmlConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyMain {
 
 	public static void main(String... args) throws Exception {
-		Server jetty = new Server();
-		String configFile = "etc/jetty.xml";
-		XmlConfiguration configuration = new XmlConfiguration(new File(configFile).toURI().toURL());
-		configuration.configure(jetty);
-		jetty.start();
+//		Server jetty = new Server();
+//		String configFile = "etc/jetty.xml";
+//		XmlConfiguration configuration = new XmlConfiguration(new File(configFile).toURI().toURL());
+//		configuration.configure(jetty);
+//		jetty.start();
+		
+		String webappDirLocation = "src/main/webapp/";
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        Server server = new Server(Integer.valueOf(webPort));
+        WebAppContext root = new WebAppContext();
+        root.setContextPath("/");
+        root.setDescriptor(webappDirLocation+"/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+        root.setParentLoaderPriority(true);
+        server.setHandler(root);
+        server.start();
+        server.join(); 
 	}
 }
