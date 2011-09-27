8ce3af96fab564c4815cd894240d2b9a66a68f4a
johnlreyes <JohnLReyes@yahoo.com>
2011-09-28 07:32:37 +0800

    port

--- web/src/main/java/per/search/server/JettyMain.java
+++ web/src/main/java/per/search/server/JettyMain.java
@@ -13,11 +13,10 @@ public class JettyMain {
 		// The port that we should run on can be set into an environment
 		// variable
 		// Look for that variable and default to 8080 if it isn't there.
-//		String webPort = System.getenv("PORT");
-//		if (webPort == null || webPort.isEmpty()) {
-//			webPort = "8080";
-//		}
-		String webPort = "8080";
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
 		
 		Server server = new Server(Integer.valueOf(webPort));
 		WebAppContext root = new WebAppContext();
